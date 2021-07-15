ae7de40bfb06791e2b50dc928309887374e41951
Reenphy George <reenphygeorge@gmail.com>
2021-07-15 15:44:05 +0530

    Button issue fixed

---
+++ script.js
@@ -0,0 +1,39 @@
+function chem_amt() {
+    window.open("https://meet.google.com/ryx-sftb-vkf", "_blank");
+}
+
+function chem_ash() {
+    window.open("https://meet.google.com/raz-vpop-iyw", "_blank");
+}
+
+function cp() {
+    window.open("https://meet.google.com/yga-hqyx-bgt", "_blank");
+}
+
+function bee() {
+    window.open("https://meet.google.com/vng-peat-pbj", "_blank");
+}
+
+function bec() {
+    window.open("https://meet.google.com/tse-ncyr-njs", "_blank");
+}
+
+function maths() {
+    window.open("https://meet.google.com/euz-icfp-img", "_blank");
+}
+
+function eg() {
+    window.open("https://meet.google.com/hsh-ocyv-wen", "_blank");
+}
+
+function pcsj() {
+    window.open("https://meet.google.com/pvo-bdjc-wvy", "_blank");
+}
+
+function pcsr() {
+    window.open("https://meet.google.com/fov-kjyv-hnk", "_blank");
+}
+
+function pcfr() {
+    window.open("#", "_blank");
+}
